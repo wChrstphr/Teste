4ca82553a0aca782e7fca99cfe86111c30c80b42
Wanjo Christopher <wanjo.christopher@gmail.com>
2023-08-29 23:11:18 -0300

    Meu primeiro commit de java!

---
+++ Teste/src/primeiro/MeuPrimeiroPrograma.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package primeiro;
+
+/**
+ * 
+ */
+public class MeuPrimeiroPrograma {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Meu primeiro programa em java!");
+
+	}
+
+}
